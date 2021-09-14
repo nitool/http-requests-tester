3ce4335adc20edfd76c53d6417d8c8c9fe62e10a
Sebastian Niecikowski <snitool@wp.pl>
2021-09-14 19:55:16 +0200

    move functional tests to proper directory

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-node http-requests-tester --selected-client test --client-file rest-client.env.json ./tests/test_case_with_variables.http
-node http-requests-tester ./tests/test_case_with_variables.http --selected-client test --client-file rest-client.env.json
-node http-requests-tester ./tests/test_case_flow.http --selected-client test --client-file rest-client.env.json
-node http-requests-tester ./tests/test_case_without_headers.http --selected-client test --client-file rest-client.env.json
-node http-requests-tester ./tests/test_case_with_external_script.http --selected-client test --client-file rest-client.env.json
-node http-requests-tester ./tests/test_case_with_dynamic.http --selected-client test --client-file rest-client.env.json
-node http-requests-tester ./tests/test_case_with_body_from_file.http --selected-client test --client-file rest-client.env.json
-node http-requests-tester ./tests/test_case_with_form_data.http --selected-client test --client-file rest-client.env.json
-node http-requests-tester ./tests/test_case_heavy.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester --selected-client test --client-file rest-client.env.json ./tests/functional/test_case_with_variables.http
+node http-requests-tester ./tests/functional/test_case_with_variables.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester ./tests/functional/test_case_flow.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester ./tests/functional/test_case_without_headers.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester ./tests/functional/test_case_with_external_script.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester ./tests/functional/test_case_with_dynamic.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester ./tests/functional/test_case_with_body_from_file.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester ./tests/functional/test_case_with_form_data.http --selected-client test --client-file rest-client.env.json
+node http-requests-tester ./tests/functional/test_case_heavy.http --selected-client test --client-file rest-client.env.json
 
